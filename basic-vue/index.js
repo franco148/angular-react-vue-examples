865be72d8274656d4d79d98d0f7b95fbbc092b42
franco-extreme <franck_ral@hotmail.com>
2019-02-13 23:21:36 -0400

    VueJs-GettingStarted: Outputting lists.

--- basic-vue/index.js
+++ basic-vue/index.js
@@ -1,11 +1,15 @@
 new Vue({
     el: '#app',
     data: {
-        name: 'Franco'
+        name: 'Franco',
+        elements: []
     },
     methods: {
         changeName: function() {
             this.name = 'Franco Arratia';
+        },
+        addElement: function() {
+            this.elements.push(this.elements.length + 1);
         }
     }
 });
