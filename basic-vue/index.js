ed39d16f0c94afa7f6a1c3277d3a8f8d0fa0fa49
franco-extreme <franck_ral@hotmail.com>
2019-02-13 23:10:15 -0400

    VueJs-GettingStarted: Handling events and updating the DOM.

--- basic-vue/index.js
+++ basic-vue/index.js
@@ -2,5 +2,10 @@ new Vue({
     el: '#app',
     data: {
         name: 'Franco'
+    },
+    methods: {
+        changeName: function() {
+            this.name = 'Franco Arratia';
+        }
     }
 });
