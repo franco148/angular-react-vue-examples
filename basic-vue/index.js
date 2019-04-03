ffe7446179042aff847e0c1d322b3431e45f304f
Franco Arratia <farratia@aptos.com>
2019-04-03 09:36:06 -0400

    VueJs-GettingStarted: Limitations of multiple VUE instances.

--- basic-vue/index.js
+++ basic-vue/index.js
@@ -17,9 +17,26 @@ new Vue({
     }
 });
 
+// Has multiple VUE instances has one potencial downside though.
+// What if you want to really use your view instance.
+
 new Vue({
     el: '#app2',
     data: {
         message: 'Hello there'
+    }
+});
+
+
+// Even if we have more than one use of this vue instance in the HTML,
+// we will see only one html element there. 
+// It will only select and control the first occurrence of this selector
+// it finds. The second one is simply ignored. A view instance can only
+// control one part of your code, not multiple ones but there is a solution
+// which actually allows us to achieve what we want. It is using COMPONENTS.
+new Vue({
+    el: '.username',
+    data: {
+        username: 'Franco'
     }
 });
