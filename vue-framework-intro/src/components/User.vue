b015845d561e91af732015dbdeab6393d938559b
Franco Arratia <franck_ral@hotmail.com>
2019-04-04 11:36:18 -0400

    VueJs-CliProject: Creating global components with .vue files.

---
+++ vue-framework-intro/src/components/User.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="user">
+        <h3>The User</h3>
+        <p>My username is {{ username }}</p>
+    </div>
+</template>
+
+<script>
+    // The exported object is just a Vue component, so you may
+    // use all the instance properties like data, props, methods, etc.
+    export default {
+        data() {
+            return {
+                username: 'Franco!!!'
+            }
+        }
+    }
+</script>
+
+<style>
+    p {
+        font-style: italic;
+    }
+</style>
+
